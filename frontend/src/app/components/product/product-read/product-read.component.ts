070b0b6cedbca97c2ffddb4fa08fa63800e27b0a
allyson <allyson.brandao@venhapranuvem.com.br>
2020-11-29 17:54:21 -0300

    Criado tabela de produtos.

--- frontend/src/app/components/product/product-read/product-read.component.ts
+++ frontend/src/app/components/product/product-read/product-read.component.ts
@@ -1,22 +1,22 @@
+import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../product.models';
-import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-read',
-  templateUrl:'./product-read.component.html',
-  styles: ['./product-read.component.css']
+  templateUrl: './product-read.component.html',
+  styleUrls: ['./product-read.component.css']
 })
 export class ProductReadComponent implements OnInit {
 
   products: Product[] = [];
-
-  constructor(private ProductService: ProductService) { }
+  displayedColumns = ['id', 'name', 'price', 'action']
+  
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
-    this.ProductService.read().subscribe(products=>{
-      this.products = products;
-      //console.log(products);
+    this.productService.read().subscribe(products => {
+      this.products = products
     })
   }
 
